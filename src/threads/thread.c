c8e1d9a2ff85343fd909fd5638987a46b012d9c4
mingracle2 <cminki02@postech.ac.kr>
2021-10-03 10:03:41 -0700

    set initial priority_max

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -518,6 +518,7 @@ init_thread (struct thread *t, const char *name, int priority)
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
+  t->priority_max = priority;
   t->magic = THREAD_MAGIC;
 
   old_level = intr_disable ();
